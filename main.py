584f4eb62776334d71a94e2df94e96e819e55f54
Matthew Bauer <bauermatt18@gmail.com>
2023-05-13 01:01:12 -0700

    Updated styling and added a window for video importation

--- main.py
+++ main.py
@@ -1,106 +1,164 @@
 import sys
-from PyQt5.QtCore import Qt, QUrl, QTimer
+from PyQt5.QtWidgets import QApplication, QFileDialog, QVBoxLayout, QSlider, QPushButton, QWidget, QMainWindow, QHBoxLayout, QStyle, QStyleOptionSlider
+from PyQt5.QtCore import QDir, Qt, QUrl
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QVBoxLayout, QSlider, QAction, QFileDialog, QLabel, QSizePolicy
 
 
 class VideoPlayer(QMainWindow):
-
-    def __init__(self):
-        super().__init__()
-
-        # Create a QMediaPlayer object
+    def __init__(self, fileName, parent=None):
+        super(VideoPlayer, self).__init__(parent)
+        self.setWindowTitle("Video Player")
         self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        videoWidget = QVideoWidget()
 
-        # Create a QVideoWidget object
-        self.videoWidget = QVideoWidget()
+        self.playButton = QPushButton()
+        self.playButton.setEnabled(False)
+        self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+        self.playButton.setStyleSheet("""
+            QPushButton {
+                border: 2px solid #555;
+                border-radius: 11px;
+                background-color: #555;
+                min-width: 80px;
+                color: #ddd;
+            }
 
-        # Rotation value
-        self.rotation = 0
+            QPushButton:hover {
+                background-color: #777;
+            }
+        """)
 
-        # Create Open button
-        openBtn = QPushButton('Open Video')
-        openBtn.clicked.connect(self.openFile)
+        self.playButton.clicked.connect(self.play)
+        self.positionSlider = QSlider(Qt.Horizontal)
+        self.positionSlider.setRange(0, 0)
+        self.positionSlider.sliderMoved.connect(self.setPosition)
+        self.positionSlider.setStyleSheet("""
+            QSlider::groove:horizontal {
+                border: 1px solid #999;
+                height: 8px;
+                background: #555;
+                margin: 2px 0;
+                border-radius: 4px;
+            }
 
-        # Create Rotate button
-        rotateBtn = QPushButton('Rotate Video')
-        rotateBtn.clicked.connect(self.rotateVideo)
+            QSlider::handle:horizontal {
+                background: #ddd;
+                border: 1px solid #777;
+                width: 13px;
+                margin: -2px 0;
+                border-radius: 7px;
+            }
 
-        # Create Play button
-        self.playBtn = QPushButton()
-        self.playBtn.setEnabled(False)
-        self.playBtn.setIcon(
-            self.style().standardIcon(self.style().SP_MediaPlay))
-        self.playBtn.clicked.connect(self.play)
+            QSlider::handle:horizontal:hover {
+                background: #bbb;
+            }
 
-        # Create a QSlider
-        self.slider = QSlider(Qt.Horizontal)
-        self.slider.setRange(0, 0)
-        self.slider.sliderMoved.connect(self.setPosition)
+            QSlider::sub-page:horizontal {
+                background: #777;
+                border: 1px solid #777;
+                height: 10px;
+                border-radius: 2px;
+            }
+        """)
 
-        # Create a QLabel
-        self.label = QLabel()
-        self.label.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
-
-        # Create a QVBoxLayout
-        layout = QVBoxLayout()
-        layout.addWidget(openBtn)
-        layout.addWidget(rotateBtn)
-        layout.addWidget(self.playBtn)
-        layout.addWidget(self.videoWidget)
-        layout.addWidget(self.slider)
-        layout.addWidget(self.label)
-
-        # Create a QWidget
-        widget = QWidget()
-        widget.setLayout(layout)
-
-        self.setCentralWidget(widget)
-
-        # Media player signals
-        self.mediaPlayer.setVideoOutput(self.videoWidget)
+        self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(fileName)))
+        self.mediaPlayer.setVideoOutput(videoWidget)
         self.mediaPlayer.positionChanged.connect(self.positionChanged)
         self.mediaPlayer.durationChanged.connect(self.durationChanged)
+        self.mediaPlayer.error.connect(self.handleError)
         self.mediaPlayer.mediaStatusChanged.connect(self.mediaStatusChanged)
+        self.playButton.setEnabled(True)
 
-    def openFile(self):
-        fileName, _ = QFileDialog.getOpenFileName(self, "Open Movie")
-        if fileName != '':
-            self.mediaPlayer.setMedia(
-                QMediaContent(QUrl.fromLocalFile(fileName)))
-            self.playBtn.setEnabled(True)
+        widget = QWidget(self)
+        widget.setStyleSheet("Background-color: #333")
+        self.setCentralWidget(widget)
+        layout = QVBoxLayout()
+        controlLayout = QHBoxLayout()
+        layout.addWidget(videoWidget)
+        controlLayout.addWidget(self.playButton)
+        controlLayout.addWidget(self.positionSlider)
+        layout.addLayout(controlLayout)
+        widget.setLayout(layout)
+
+    def play(self):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.mediaPlayer.pause()
+            self.playButton.setIcon(
+                self.style().standardIcon(QStyle.SP_MediaPlay))
+        else:
+            self.mediaPlayer.play()
+            self.playButton.setIcon(
+                self.style().standardIcon(QStyle.SP_MediaPause))
+
+    def positionChanged(self, position):
+        self.positionSlider.setValue(position)
+
+    def durationChanged(self, duration):
+        self.positionSlider.setRange(0, duration)
+
+    def setPosition(self, position):
+        self.mediaPlayer.setPosition(position)
+
+    def handleError(self):
+        self.playButton.setEnabled(False)
+        print("Error: " + self.mediaPlayer.errorString())
 
     def mediaStatusChanged(self, status):
         if status == QMediaPlayer.LoadedMedia:
             self.mediaPlayer.pause()
 
-    def play(self):
-        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
-            self.mediaPlayer.pause()
-        else:
-            self.mediaPlayer.play()
 
-    def rotateVideo(self):
-        self.rotation = (self.rotation + 90) % 360
-        self.mediaPlayer.setVideoOutput(None)
-        self.mediaPlayer.setVideoOutput(self.videoWidget)
-        self.videoWidget.setAspectRatioMode(Qt.IgnoreAspectRatio)
-        self.videoWidget.rotate(self.rotation)
+class InitialWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Import Video')
+        self.resize(800, 600)  # Resize the window
 
-    def positionChanged(self, position):
-        self.slider.setValue(position)
+        instructionLabel = QLabel(
+            "Please click the below button to import the video that you wish to play.")
+        instructionLabel.setStyleSheet(
+            "color: #ddd;")  # Set the color of the text
 
-    def durationChanged(self, duration):
-        self.slider.setRange(0, duration)
+        openButton = QPushButton("Import Video")
+        openButton.setStyleSheet("""
+            QPushButton {
+                border: 2px solid #555;
+                border-radius: 11px;
+                background-color: #555;
+                min-width: 200px;  # Make the button larger
+                min-height: 50px;  # Make the button larger
+                color: #ddd;
+                font-size: 20px;  # Increase the font size
+            }
 
-    def setPosition(self, position):
-        self.mediaPlayer.setPosition(position)
+            QPushButton:hover {
+                background-color: #777;
+            }
+        """)
+
+        openButton.clicked.connect(self.openFile)
+
+        layout = QVBoxLayout()
+        # Add the instruction text
+        layout.addWidget(instructionLabel, alignment=Qt.AlignCenter)
+        # Center the button
+        layout.addWidget(openButton, alignment=Qt.AlignCenter)
+        self.setLayout(layout)
+        self.setStyleSheet("Background-color: #333")
+
+    def openFile(self):
+        fileName, _ = QFileDialog.getOpenFileName(
+            self, "Import Video", QDir.homePath())
+        if fileName != '':
+            self.player = VideoPlayer(fileName)
+            self.player.resize(640, 480)
+            self.player.show()
+            self.close()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    player = VideoPlayer()
-    player.resize(640, 480)
-    player.show()
+    initial_window = InitialWindow()
+    initial_window.show()
     sys.exit(app.exec_())
